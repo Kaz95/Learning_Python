b4040612c2ef555174e60548e383cbf77134b83a
Kaz95 <smurfrektshrekt@gmail.com>
2019-01-05 19:09:00 -0600

    2nd Commit - Chapter 5 example file     - Character counter

---
+++ Chapter 5 - Dictionaries and Struturing Data/Examples/pretty_character_count.py
@@ -0,0 +1,13 @@
+# Program that iterates over the given string value and prints the number of occurrences of each character in the string
+# Capital characters count separate from lower case.
+
+import pprint   # Formats dictionary output
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+
+count = {}  # Empty dictionary. Used to keep track of character occurrences in the form of key:value pairs.
+
+for character in message:   # Iterate over all characters in a string(including spaces!).
+    count.setdefault(character, 0)  # Checks if character has already occurred. If not, add to dictionary as (key: 0)
+    count[character] = count[character] + 1  # Adds 1 to the current characters count. Would return error without above.
+pprint.pprint(count)  # Capital characters count separate from lower case. pprint.pformat to return as string.
