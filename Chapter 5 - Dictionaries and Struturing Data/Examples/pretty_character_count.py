e800e5a4d216f91ae27e0cec4d3a00dbd0f1e930
Kaz95 <smurfrektshrekt@gmail.com>
2019-01-07 15:50:16 -0600

    Chapter 6 Update - Notes updated

--- Chapter 5 - Dictionaries and Struturing Data/Examples/pretty_character_count.py
+++ Chapter 5 - Dictionaries and Struturing Data/Examples/pretty_character_count.py
@@ -9,5 +9,9 @@ count = {}  # Empty dictionary. Used to keep track of character occurrences in t
 
 for character in message:   # Iterate over all characters in a string(including spaces!).
     count.setdefault(character, 0)  # Checks if character has already occurred. If not, add to dictionary as (key: 0)
-    count[character] = count[character] + 1  # Adds 1 to the current characters count. Would return error without above.
-pprint.pprint(count)  # Capital characters count separate from lower case. pprint.pformat to return as string.
+    count[character] += 1  # Adds 1 to the current characters count. Would return error without above.
+
+
+"""Capital characters count separate from lower case. Pass to .upper or .lower method first to prevent this. 
+    pprint.pformat to return as string for further use in different string methods"""
+pprint.pprint(count)
