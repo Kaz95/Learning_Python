dea746c03b9a47fea18ad6f51c6d905804497f5b
Kaz95 <smurfrektshrekt@gmail.com>
2019-02-02 23:04:02 -0600

    Chapter 7 Update

--- Chapter 6 - Manipulating Strings/Chapter Project - Password Locker/pw.py
+++ Chapter 6 - Manipulating Strings/Chapter Project - Password Locker/pw.py
@@ -1,7 +1,8 @@
 #! python 3
-# TODO Make .bat file
+
 # pw.py - An insecure password locker program.
 import sys
+import pyperclip
 
 passwords = {'email': 'email password', 'blog': 'blog password', 'luggage': 'luggage password'}
 
@@ -9,4 +10,10 @@ if len(sys.argv) < 2:   # If no input. argv automatically has program name as fi
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()  # Exits loop once argument received.
 
-account = sys.argv[1]
+account = sys.argv[1]   # account = first argument passed to cmd line
+
+if account in passwords:
+    pyperclip.copy(passwords[account])
+    print('Password for ' + account + ' copied to clipboard')
+else:
+    print('There is no password for ' + account)
